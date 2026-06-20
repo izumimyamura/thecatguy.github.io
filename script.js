e342cd97eaf8c21a9c44602cd052c17b8cec7f93
THECATGUY <kevin123kevinl123@gmail.com>
2026-06-20 20:33:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,48 +2,18 @@
 
 const glow = document.querySelector(".cursor-glow");
 
-document.addEventListener("mousemove", e => {
-  glow.style.left = e.clientX + "px";
-  glow.style.top = e.clientY + "px";
+document.addEventListener("mousemove",(e)=>{
+
+    glow.style.left = e.clientX + "px";
+    glow.style.top = e.clientY + "px";
+
 });
 
-// Animated Text Reveal
+// Text Reveal
 
 gsap.from(".reveal",{
-  y:100,
-  opacity:0,
-  duration:1.5,
-  stagger:0.2
-});
-
-// Scroll Animation
-
-gsap.utils.toArray(".card").forEach(card => {
-
-  gsap.from(card,{
-    scrollTrigger:{
-      trigger:card,
-      start:"top 80%"
-    },
     y:100,
     opacity:0,
-    duration:1
-  });
-
-});
-
-// Hover Play Video
-
-document.querySelectorAll(".card").forEach(card=>{
-
-  const video = card.querySelector("video");
-
-  card.addEventListener("mouseenter",()=>{
-    video.play();
-  });
-
-  card.addEventListener("mouseleave",()=>{
-    video.pause();
-  });
-
+    duration:1.5,
+    stagger:0.2
 });
