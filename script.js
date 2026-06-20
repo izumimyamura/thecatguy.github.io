9486334d5e0fa8dfd0305cc414cc79e8f29a3229
THECATGUY <kevin123kevinl123@gmail.com>
2026-06-20 22:26:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const glow = document.querySelector(".cursor-glow");
 
-document.addEventListener("mousemove",(e)=>{
+document.addEventListener("mousemove", (e) => {
 
     glow.style.left = e.clientX + "px";
     glow.style.top = e.clientY + "px";
@@ -11,9 +11,23 @@ document.addEventListener("mousemove",(e)=>{
 
 // Text Reveal
 
-gsap.from(".reveal",{
+gsap.from(".reveal", {
     y:100,
     opacity:0,
     duration:1.5,
     stagger:0.2
+});
+
+// Cassette Rotation
+
+const cassette = document.querySelector(".cassette");
+
+document.addEventListener("mousemove", (e) => {
+
+    const x = (e.clientX / window.innerWidth - 0.5) * 20;
+    const y = (e.clientY / window.innerHeight - 0.5) * 20;
+
+    cassette.style.transform =
+        `rotateY(${x}deg) rotateX(${-y}deg)`;
+
 });
